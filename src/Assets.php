ac3bbe53d79812c3f9c2765cd7b7c1bba3b09179
Alex <aleshaascet@gmail.com>
2025-05-14 17:05:17 +0300

    install introvert api

--- src/Assets.php
+++ src/Assets.php
@@ -1,12 +1,33 @@
 <?php
 
-namespace IntrivertTest2;
+namespace IntrovertTest;
 
 class Assets
 {
 
-    public  function getAssets()
+    /**
+     * @return string[]
+     *
+     * фнкция возращает массив пакетов css и js
+     * Более продвинутом варианте, конечно  можно сделать с выводом js в разных
+     * частях станицы HEAD BODY END - если js как то влияет от порядка подключения CSS
+     * классов или DOM объектов
+     * P.s начианаю жалеть что не использую  Yii2 -> не было бы этих
+     * заморочек с подключения ассетс и роутов
+     */
+    static function getAssets()
     {
-        return [];
+        return [
+           'css'=>' <link href="/src/assets/glDatePicker-2.0/styles/glDatePicker.default.css" rel="stylesheet" type="text/css">',
+           'js'=>[
+               '<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>',
+               '<script src="/src/assets/glDatePicker-2.0/glDatePicker.js"></script>',
+            ],
+        ];
+    }
+
+    static function registerAssets()
+    {
+        self::getAssets();
     }
 }
--- src/IntrovertApi.php
+++ src/IntrovertApi.php
@@ -2,7 +2,37 @@
 
 namespace IntrovertTest;
 
+use Introvert\ApiClient;
 class IntrovertApi
 {
+    const           API_URL = 'https://api.introvert.com/api/';
+    protected const API_KEY = "23bc075b710da43f0ffb50ff9e889aed";
+
+
+    public function __construct()
+    {
+        Introvert\Configuration::getDefaultConfiguration()->setApiKey('key', self::API_KEY);
+        $api = new Introvert\ApiClient();
+        try {
+            $result = $api->account->allStatuses();
+            echo '<pre>';
+            var_dump($result);
+            echo '<pre>';
+        } catch (Exception $e) {
+            echo 'Exception when calling account->allStatuses: ', $e->getMessage(), PHP_EOL;
+        }
+    }
+
+    /**
+     * @param $count
+     * @param $offset
+     * @return mixed
+     * скопировал с предыдущего задания
+     */
+    private function getLeads($count, $offset)
+    {
+        $res = $this->api->lead->getAll([], [], [], '', $count, $offset);
+        return $res['result'];
+    }
 
 }
